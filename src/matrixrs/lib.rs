c23ac2851c687eb1b448c1db176b0f0cc5f7d039
Young Woo <doomsplayer@gmail.com>
2014-06-15 23:17:43 +0800

    update to v0.2

--- src/matrixrs/lib.rs
+++ src/matrixrs/lib.rs
@@ -1,18 +1,12 @@
-#![crate_id = "matrixrs#0.1"];
-#![crate_type="lib"];
-#![allow(unused_imports)]
-#![allow(dead_code)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case_functions)]
-#![allow(uppercase_variables)] 
+#![crate_id = "matrixrs#0.2"]
+#![crate_type="lib"]
+#![allow(unused_must_use)]
 
-extern crate std;
 use std::num;
-use std::num::abs;
-use std::cmp::{Eq, TotalOrd};
-use std::owned::Box;
-use std::fmt::Formatter;
-
+use std::num::{abs,zero,one,Zero};
+use std::cmp::{PartialOrd};
+use std::fmt::{Formatter,Show};
+use std::vec::Vec;
 /// Matrix -- Generic 2D Matrix implementation in Rust.
 pub struct Matrix<T> {
 	/// Number of rows
@@ -31,13 +25,13 @@ impl<T> Matrix<T> {
 		//!
 		//! ```rust
 		//! # use matrixrs::Matrix;
-		//! assert_eq!(Matrix::from_fn(2, 2, |i,j| { i+j }), Matrix{m:2,n:2,data:~[~[0,1],~[1,2]]});
+		//! assert_eq!(Matrix::from_fn(2, 2, |i,j| i+j), Matrix{ m: 2, n: 2, data: vec![vec![0,1], vec![1,2]]});
 		//! ```
-		let mut data = Vec::with_capacity(row);
-		for i in range(0, row) {
-			data.push(Vec::from_fn(col, |j:uint| -> T { func(i, j) }));
+		Matrix {
+			row: row,
+			col: col,
+			data: range(0, row).map(|i| Vec::from_fn(col, |j| func(i, j))).collect()
 		}
-		Matrix{row: row, col: col, data:data}
 	}
 
 	pub fn size(&self) -> (uint, uint) {
@@ -47,36 +41,48 @@ impl<T> Matrix<T> {
 }
 
 impl<T:Clone> Matrix<T> {
-	pub fn from_T(row : uint, col : uint, val : T) -> Matrix<T> {
+	pub fn from_elem(row: uint, col: uint, val: T) -> Matrix<T> {
 		//! Create an m-by-n matrix, where each element is a clone of val.
 		//!
 		//! ```rust
 		//! # use matrixrs::Matrix;
-		//! assert_eq!(Matrix::from_T(2, 2, 10), Matrix{m:2,n:2,data:~[~[10,10],~[10,10]]});
+		//! assert_eq!(Matrix::from_elem(2, 2, 10), Matrix { m: 2,n: 2,data: vec![vec![10,10], vec![10,10]]});
 		//! ```
-		let mut data = Vec::with_capacity(row);
-		for _ in range(0, row) {
-			data.push(Vec::from_elem(col, val.clone()));
+
+		Matrix {
+			row: row,
+			col: col,
+			data: range(0, row).map(|_| Vec::from_elem(col, val.clone())).collect()
 		}
-		Matrix{row: row, col: col, data:data}
 	}
 	//pub fn from_diag(diag : ~[T], k : int)
-	pub fn at(&self, row : uint, col : uint) -> T {
+	pub fn at(&self, row: uint, col: uint) -> T {
 		//! Return the element at row, col.
 		//! Wrapped by Index trait.
 		self.data.get(row).get(col).clone()
 	}
-	pub fn row(&self, row : uint) -> Matrix<T> {
+	pub fn row_vec(&self, row: uint) -> Vec<T> {
+		let mut v = Vec::new();
+		v.clone_from(self.data.get(row));
+		v
+	}
+	pub fn row(&self, row: uint) -> Matrix<T> {
 		//! Return specified row from an MxN matrix as a 1xN matrix.
 		//!
 		//! ```rust
 		//! # use matrixrs::Matrix;
 		//! assert_eq!(Matrix{m:2,n:1,data:~[~[1],~[2]]}.row(0), Matrix{m:1,n:1,data:~[~[1]]});
 		//! ```
-
-		Matrix{row: 1, col: self.col, data: vec![Vec::from_slice(self.data.get(row).as_slice())]}
+		Matrix{row: 1, col: self.col, data: vec!(self.row_vec(row))}
 	}
-	pub fn col(&self, col : uint) -> Matrix<T> {
+	pub fn col_vec(&self, col: uint) -> Vec<T> {
+		let mut c = Vec::with_capacity(self.row);
+		for i in range(0, self.row) {
+			c.push(self.at(i, col));
+		}
+		c
+	}
+	pub fn col(&self, col: uint) -> Matrix<T> {
 		//! Return specified col from an MxN matrix as an Mx1 matrix.
 		//!
 		//! ```rust
@@ -90,11 +96,15 @@ impl<T:Clone> Matrix<T> {
 		Matrix{row: self.row, col: 1, data: c}
 	}
 	//pub fn diag(&self, k : int) -> Matrix<T>
-	pub fn augment(&self, mat : &Matrix<T>) -> Matrix<T> {
+	pub fn augment(&self, mat: &Matrix<T>) -> Matrix<T> {
 		//! Return a new matrix, self augmented by matrix mat.
 		//! An MxN matrix augmented with an MxC matrix produces an Mx(N+C) matrix.
 		Matrix::from_fn(self.row, self.col+mat.col, |i,j| {
-			if j < self.col { self.at(i, j) } else { mat.at(i, j - self.col) }
+			if j < self.col {
+				self.at(i, j) 
+			} else {
+				mat.at(i, j - self.col) 
+			}
 		})
 	}
 	pub fn transpose(&self) -> Matrix<T> {
@@ -111,22 +121,47 @@ impl<T:Clone> Matrix<T> {
 			}
 		}
 	}
-	pub fn fold(&self, init : T, folder: |T,T| -> T) -> T {
+	pub fn fold(&self, init: T, folder: |T,T| -> T) -> T {
 		//! Call a folder function that acts as if it flattens the matrix
 		//! onto one row and then folds across.
 		let mut acc = init;
 		self.apply(|i,j| { acc = folder(acc.clone(), self.at(i,j)); });
+
 		acc
 	}
 }
 
-impl<T:std::fmt::Show> std::fmt::Show for Matrix<T> {
+impl<T:Show> Show for Matrix<T> {
 	fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-		// write!(fmt, "\n");
+		let max_width =
+		self.data.iter().fold(0, |maxlen, rowvec| {
+			let rowmax = rowvec.iter().fold(0, |maxlen, elem| {
+				let l = format!("{}", elem).len();
+				if maxlen > l {
+					maxlen
+				} else {
+					l
+				}
+			});
+
+			if maxlen > rowmax {
+				maxlen
+			} else {
+				rowmax
+			}
+		});
+
+		
+		write!(fmt, "{}", "\n");
 		for i in self.data.iter() {
 			write!(fmt, "|");
 			for v in i.iter() {
-				write!(fmt, " {}", v);
+				let slen = format!("{}", v).len();
+				let mut padding = " ".to_str();
+				for _ in range(0, max_width-slen) {
+					padding.push_str(" ");
+				}
+				write!(fmt, "{}{}", padding, v);
 			}
 			write!(fmt, " |\n");
 		}
@@ -144,7 +179,7 @@ impl<T:Clone, U> Matrix<T> {
 }
 
 // methods for Matrix of numbers
-impl<T:Add<T,T>+Mul<T,T>+num::Zero+Clone> Matrix<T> {
+impl<T:Add<T,T>+Mul<T,T>+Zero+Clone> Matrix<T> {
 	pub fn sum(&self) -> T {
 		//! Return the summation of all elements in self.
 		//!
@@ -152,15 +187,11 @@ impl<T:Add<T,T>+Mul<T,T>+num::Zero+Clone> Matrix<T> {
 		//! # use matrixrs::Matrix;
 		//! assert_eq!(Matrix{m:2,n:2,data:~[~[1,3],~[2,4]]}.sum(), 10);
 		//! ```
-		self.fold(num::zero(), |a,b| { a + b })
+		self.fold(zero(), |a,b| { a + b })
 	}
 	fn dot(&self, other: &Matrix<T>) -> T {
 		//! Return the product of the first row in self with the first row in other.
-		let mut sum : T = num::zero();
-		for i in range(0, self.col) {
-			sum = sum + self.at(0, i) * other.at(i, 0);
-		}
-		sum
+		range(0, self.col).fold(zero(), |acc: T, i| acc + self.at(0, i) * other.at(i, 0))
 	}
 }
 
@@ -172,7 +203,7 @@ impl<T:NumCast+Clone> Matrix<T> {
 }
 
 impl<T:NumCast+Clone, U:NumCast+Clone> Matrix<T> {
-	pub fn approx_eq(&self, other: &Matrix<U>, threshold : f64) -> bool {
+	pub fn approx_eq(&self, other: &Matrix<U>, threshold: f64) -> bool {
 		//! Return whether all of the elements of self are within
 		//! threshold of all of the corresponding elements of other.
 		let other_f64 = other.to_f64();
@@ -189,13 +220,13 @@ impl<T:NumCast+Clone, U:NumCast+Clone> Matrix<T> {
 	}
 }
 
-impl<T:Num+NumCast+Clone+Signed+TotalOrd> Matrix<T> {
+impl<T:Num+NumCast+Clone+Signed+PartialOrd> Matrix<T> {
 	fn doolittle_pivot(&self) -> Matrix<T> {
 		//! Return the pivoting matrix for self (for Doolittle algorithm)
 		//! Assume that self is a square matrix.
 		// initialize with a type T identity matrix
 		let mut pivot = Matrix::from_fn(self.row, self.col, |i, j| {
-			if i == j { num::one() } else { num::zero() }
+			if i == j { one() } else { zero() }
 		});
 		// rearrange pivot matrix so max of each column of self is on
 		// the diagonal of self when multiplied by the pivot
@@ -206,60 +237,68 @@ impl<T:Num+NumCast+Clone+Signed+TotalOrd> Matrix<T> {
 					row_max = i;
 				}
 			}
+
 			// swap the maximum row with the current one
-			let tmp = Vec::from_slice(pivot.data.as_slice()[j].as_slice());
-			pivot.data.as_mut_slice()[j] = Vec::from_slice(pivot.data.as_slice()[row_max].as_slice());
+			let tmp = pivot.row_vec(j);
+			pivot.data.as_mut_slice()[j] = pivot.row_vec(row_max);
 			pivot.data.as_mut_slice()[row_max] = tmp;
 		}
 		pivot
 	}
-	pub fn lu(&self) -> (Matrix<T>, Matrix<f64>, Matrix<f64>) {
+	pub fn lu(&self) -> Result<(Matrix<T>, Matrix<f64>, Matrix<f64>),String> {
 		//! Perform the LU decomposition of square matrix self, and return
 		//! the tuple (P,L,U) where P*self = L*U, and L and U are triangular.
+		if self.col != self.row {
+			return Err("col num don't match row num".to_str());
+		}
 		assert_eq!(self.row, self.col);
-		let P = self.doolittle_pivot();
-		let PM = (P*(*self)).to_f64();
-		let mut L = identity(self.row);
-		let mut U = zeros(self.row, self.col);
+		let p = self.doolittle_pivot();
+		let pm = (p*(*self)).to_f64();
+		let mut l = eye(self.row);
+		let mut u = zeros(self.row, self.col);
 		for j in range(0, self.col) {
 			for i in range(0, j+1) {
 				let mut uppersum = 0.0;
 				for k in range(0,i) {
-					uppersum += U.at(k,j)*L.at(i,k);
+					uppersum += u.at(k,j)*l.at(i,k);
 				}
-				U.data.as_mut_slice()[i].as_mut_slice()[j] = PM.at(i,j) - uppersum;
+				u.data.as_mut_slice()[i].as_mut_slice()[j] = pm.at(i,j) - uppersum;
 			}
 			for i in range(j, self.row) {
 				let mut lowersum = 0.0;
 				for k in range(0,j) {
-					lowersum += U.at(k,j)*L.at(i,k);
+					lowersum += u.at(k,j)*l.at(i,k);
 				}
-				L.data.as_mut_slice()[i].as_mut_slice()[j] = (PM.at(i,j) - lowersum) / U.at(j,j);
+				l.data.as_mut_slice()[i].as_mut_slice()[j] = (pm.at(i,j) - lowersum) / u.at(j,j);
+				// println!("{:?}",l.at(i,j));
 			}
 		}
-		(P, L, U)
+		Ok((p, l, u))
 	}
-	pub fn det(&self) -> f64 {
+	pub fn det(&self) -> Result<f64,String> {
 		//! Return the determinant of square matrix self
 		//! via LU decomposition.
 		//! If not a square matrix, fail.
-		match self.lu() {
+		if self.col != self.row {
+			return Err("col num don't match row num".to_str());
+		}
+		match self.lu().unwrap() {
 			// |L|=1 because it L is unitriangular
 			// |P|=1 or -1 because it's a permutation matrix
 			// |U|=product of U's diagonal
-			(P, _, U) => {
+			(p, _, u) => {
 				// return the product of the diagonal
 				let mut prod = 1.0;
 				let mut swaps = 0;
 				for i in range(0, self.row) {
-					prod *= U.at(i,i);
-					swaps += if P.at(i,i) == num::one() { 0 } else { 1 };
+					prod *= u.at(i,i);
+					swaps += if p.at(i,i) == one() { 0 } else { 1 };
 				}
 				// flip the sign of the determinant based on swaps of P
 				if (swaps/2) % 2 == 1 {
-					-prod
+					Ok(-prod)
 				} else {
-					prod
+					Ok(prod)
 				}
 			}
 		}
@@ -298,7 +337,7 @@ impl<T:Neg<T>+Add<T,T>+Clone> Sub<Matrix<T>, Matrix<T>> for Matrix<T> {
 }
 
 // use * to multiply matrices
-impl<T:Add<T,T>+Mul<T,T>+num::Zero+Clone> Mul<Matrix<T>, Matrix<T>> for Matrix<T> {
+impl<T:Add<T,T>+Mul<T,T>+Zero+Clone> Mul<Matrix<T>, Matrix<T>> for Matrix<T> {
 	fn mul(&self, rhs: &Matrix<T>) -> Matrix<T> {
 		//! Return the product of multiplying two matrices.
 		//! MxR matrix * RxN matrix = MxN matrix.
@@ -309,6 +348,14 @@ impl<T:Add<T,T>+Mul<T,T>+num::Zero+Clone> Mul<Matrix<T>, Matrix<T>> for Matrix<T
 		})
 	}
 }
+
+//these will be comments until RFC #48 landed
+// impl<T:Add<T,T>+Mul<T,T>+Zero+Clone+ToMatrix> Mul<T, Matrix<T>> for Matrix<T> {
+// 	fn mul(&self, rhs: &T) -> Matrix<T> {
+// 		let mrhs = rhs.to_matrix(self.row, self.col);
+// 		self * mrhs
+// 	}
+// }
 
 // use [(x,y)] to index matrices
 impl<T:Clone> Index<(uint, uint), T> for Matrix<T> {
@@ -352,18 +399,30 @@ impl<T:Add<T,T>+Mul<T,T>+num::Zero+Clone> BitXor<uint, Matrix<T>> for Matrix<T> 
 	}
 }
 
+pub trait ToMatrix {
+	fn to_matrix(&self, row: uint, col: uint) -> Matrix<Self>;
+}
+
+// impl<T> ToMatrix for Martix<T> {
+
+// }
+impl ToMatrix for int {
+	fn to_matrix(&self, row: uint, col: uint) -> Matrix<int> {
+		Matrix::from_elem(row, col, self.clone())
+	}
+}
 // convenience constructors
 pub fn zeros(row : uint, col : uint) -> Matrix<f64> {
 	//! Create an MxN zero matrix of type f64.
-	Matrix::from_T(row, col, 0.0)
+	Matrix::from_elem(row, col, 0.0)
 }
 
 pub fn ones(row : uint, col : uint) -> Matrix<f64> {
 	//! Create an MxN ones matrix of type f64.
-	Matrix::from_T(row, col, 1.0)
+	Matrix::from_elem(row, col, 1.0)
 }
 
-pub fn identity(dim : uint) -> Matrix<f64> {
+pub fn eye(dim : uint) -> Matrix<f64> {
 	//! Create a dimxdim identity matrix of type f64.
 	Matrix::from_fn(dim, dim, |i, j| { if i == j { 1.0 } else { 0.0 }})
-}y
+}
